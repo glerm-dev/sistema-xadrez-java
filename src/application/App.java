0e395626c05214979a06ec5f899b90dac4e20d88
Guilherme Ferreira <gffreitas13@gmail.com>
2023-11-06 23:19:46 -0300

    adicionada a classe Position

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Position pos = new Position(0, 0);
+        System.out.println(pos);
     }
 }
